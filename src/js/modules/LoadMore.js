1dd0bd3ba430a264cc81c8a5dbdc5ba62665180e
Claudio Gandolffi <claucode88@gmail.com>
2026-03-02 03:01:17 +0100

    LoadMore.js, Render Content.js: content renderezation is being dynimically.

--- src/js/modules/LoadMore.js
+++ src/js/modules/LoadMore.js
@@ -1,38 +1,95 @@
 class LoadMore {
-  constructor(){
+  constructor() {
     this.DOM = {
       button: document.querySelector('.js--load-more'),
+      container: document.querySelector('.js--cards-container'),
+      taxonomySelect: document.querySelector('.js--taxonomy-select'),
     };
-    
+
     this.init();
     this.events();
   }
 
-  init(){
-    console.log('Init');
+  async init(){
+    await this.loadItems();
+    await this.loadTaxonomies();
   }
 
-  events(){
-    this.DOM.button?.addEventListener('click', () => {
+  events() {
+    this.DOM.button.addEventListener('click', () => this.loadItems(false));
+    
+    this.DOM.taxonomySelect?.addEventListener('change', () => {
+      this.DOM.container.innerHTML = '';
       this.loadItems();
     });
   }
 
-  async loadItems(){
-    try {
-      const response = await fetch(
-        `https://www.sei.com/wp-json/wp/v2/insight`
-      );
+  async loadTaxonomies() {
+    const taxonomies = await this.fetchTaxonomies();
+    if(!taxonomies) return;
 
-      if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`);
+    taxonomies.forEach(taxonomy => {
+      const opt = document.createElement('option');
+      opt.value = taxonomy.id;
+      opt.textContent = taxonomy.name;
+      this.DOM.taxonomySelect.appendChild(opt);
+    });
+  }
+
+  async loadItems(ctab = true){
+    // Fetching items
+    const items = await this.fetchItems();
+    if (!items) return;
+
+    // Getting content to render
+    const contentByRow = await this.getContentToRender(items, ctab);
+    if(contentByRow){
+      // this.DOM.container.innerHTML = '';
+      Object.values(contentByRow).forEach((row) => {
+        this.DOM.container.innerHTML += row;
+      })
+    }
+  }
+
+  async getContentToRender(items, ctab){
+    const response = await fetch('/api/v1/render-content',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( 
+          { data: items,
+            ctab: ctab,
+          }
+        ),
       }
+    );
 
-      const data = await response.json();
-      console.log(data);
+    if (!response.ok) throw new Error('Failed render content API!');
+    const contentData = await response.json();
+      console.log('Click: ', ctab);
+    return contentData.contentData;
+  }
+
+  async fetchItems(){
+    try {
+      const response = await fetch('https://www.sei.com/wp-json/wp/v2/insight');
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+      const items = await response.json();
+      return items;
 
     } catch (error) {
-      console.error('API error:', error);
+      console.error('LoadMore error:', error);
+    }
+  }
+
+  async fetchTaxonomies(){
+    try {
+      const response = await fetch('https://www.sei.com/wp-json/wp/v2/insight-types');
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+      const taxonomies = await response.json();
+      return taxonomies;
+    } catch (error) {
+      console.error('Taxonomy fetch error:', error);
     }
   }
 }
--- src/pages/api/v1/render-content.js
+++ src/pages/api/v1/render-content.js
@@ -1,20 +1,55 @@
-export async function POST({ request }) {
-    const container = await experimental_AstroContainer.create();
-    const { data = []} = await request.json();
-    
-    try {
-        const module = await import(`{path-to-card.astro}`);
-        const Component = module.default;
-        const result = await container.renderToString(Component, {
-            props: {
-                data: data,
-            },
-        });
+import { experimental_AstroContainer } from 'astro/container';
+import CardA from '@components/cards/CardA.astro';
+import CtaB from '@components/cta/CtaB.astro';
 
-        return new Response(JSON.stringify({ result }), {
-            headers: { "Content-Type": "application/json" },
-        });
-    } catch (err) {
-        return new Response(JSON.stringify({ error: `Error`, details: err.message }), { status: 500, headers: { "Content-Type": "application/json" } });
+export async function POST({ request }) {
+  try {
+    let contentData = { 'cardsAFirstRow': '', 'ctaBRow': '', 'cardsASecondRow': '' };
+    const maxCardsFirstRow = 3;
+    const { data = [], ctab } = await request.json();
+    const container = await experimental_AstroContainer.create();
+
+    // CardA Component
+    if (data.length) {
+      const renderPromises = data.map(item => 
+        container.renderToString(CardA, {
+          props: {
+            title: item.title.rendered,
+            subtitle: item.type,
+          },
+        })
+      );
+      const renderedCards = await Promise.all(renderPromises);
+
+      renderedCards.forEach((card, index) => {
+        let divCol = `<div class="f--col-6">${card}</div>`;
+        if (index <= maxCardsFirstRow){
+          contentData.cardsAFirstRow += divCol;
+        } else {
+          contentData.cardsASecondRow += divCol;
+        }
+      });
     }
+
+    // CtaB Component
+    if(ctab){
+      const CtaBComponent = await container.renderToString(CtaB);
+      contentData.ctaBRow = `<div class="f--col-12">${CtaBComponent}</div>`;
+    }
+
+    return new Response(JSON.stringify({ 'contentData': contentData }),
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  }catch (err) {
+    console.error("Full error:", err);
+    return new Response(
+      JSON.stringify({ error: "Error", details: err.message }),
+      { status: 500,
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+  }
 }
