4cf463cfd268af587ca3a475b653c8f60c4a0e12
Claudio Gandolffi <claucode88@gmail.com>
2026-03-03 16:28:21 +0100

    Refactor (render_content.js): handling errors done.

--- src/pages/api/v1/render-content.js
+++ src/pages/api/v1/render-content.js
@@ -3,34 +3,63 @@ import CardA from '@components/cards/CardA.astro';
 import CtaB from '@components/cta/CtaB.astro';
 
 export async function POST({ request }) {
+  let body;
+
+  // 1. Handle Json
+  try {
+    body = await request.json();
+  } catch {
+    return new Response(
+      JSON.stringify({ error: 'Invalid JSON.' }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+  }
+
+  // 2. Validate data structure
+  if(!Array.isArray(body.data)){
+    return new Response(
+      JSON.stringify(
+        { error: 'Data must be an array.' }
+      ),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" }
+      }
+    );
+  }
+
+  // 3. Main logic
   try {
     let cardsAFirstRow = '', cardsASecondRow = '', ctaBRow = '', contentData = '';
     const maxCardsFirstRow = 4;
-    const { data = [], loadMore } = await request.json();
     const container = await experimental_AstroContainer.create();
 
     // CardA Component
-    if (data.length) {
-      const renderPromises = data.map(item => 
-        container.renderToString(CardA, {
-          props: { title: item.title.rendered, subtitle: item.type },
-        })
-      );
-      const renderedCards = await Promise.all(renderPromises);
+    const renderPromises = body.data.map(item =>
+      container.renderToString(CardA, {
+        props: {
+          title: item?.title?.rendered ?? '',
+          subtitle: item?.type ?? ''
+        },
+      })
+    );
+    const renderedCards = await Promise.all(renderPromises);
 
-      // Setting rows content
-      renderedCards.forEach((card, index) => {
-        let divCol = `<div class="f--col-6">${card}</div>`;
-        if (index < maxCardsFirstRow){
-          cardsAFirstRow += divCol;
-        } else {
-          cardsASecondRow += divCol;
-        }
-      });
-    }
+    // Setting card A rows content
+    renderedCards.forEach((card, index) => {
+      let divCol = `<div class="f--col-6">${card}</div>`;
+      if (index < maxCardsFirstRow){
+        cardsAFirstRow += divCol;
+      } else {
+        cardsASecondRow += divCol;
+      }
+    });
 
     // CtaB Component
-    if(!loadMore){
+    if(!body.loadMore){
       const CtaBComponent = await container.renderToString(CtaB);
       ctaBRow = `<div class="f--col-12">${CtaBComponent}</div>`;
     }
@@ -38,16 +67,18 @@ export async function POST({ request }) {
     // Setting final content
     contentData = cardsAFirstRow + ctaBRow + cardsASecondRow;
     
-    return new Response(JSON.stringify({ 'contentData': contentData }),
+    return new Response(JSON.stringify({ contentData: contentData }),
     {
+      status: 200,
       headers: { "Content-Type": "application/json" },
     }
   );
-  }catch (err) {
-    console.error("Full error:", err);
+  }catch (error) {
+    console.error("Full error:", error);
     return new Response(
-      JSON.stringify({ error: "Error", details: err.message }),
-      { status: 500,
+      JSON.stringify({ error: "Internal server error."}),
+      {
+        status: 500,
         headers: { "Content-Type": "application/json" }
       }
     );
